44610e18f9f0ada3ca8efdc50e86fb9d4497219c
developerdesigner18 <developer.designer18@gmail.com>
2023-03-02 17:54:08 +0530

    updated profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -24,28 +24,29 @@ const Profile = () => {
             <Button className='mt-4' variant="outline-primary" onClick={() => navigate('/')}>
                 <HiOutlineArrowNarrowLeft style={{ fontSize: '1.5rem' }} />
             </Button>
-            <Container className='mt-4 profile-container'>
-                <div className='user-info'>
-                    <Row className='mt-2'>
-                        <Col className='label'>Name: <span>{user?.name}</span></Col>
-                    </Row>
-                    <Row className='mt-2'>
-                        <Col className='label'>Email: <span>{user?.email}</span></Col>
-                    </Row>
-                    <Row className='mt-2'>
-                        <Col className='label'>Token: <span>{user?.token}</span></Col>
-                    </Row>
-                </div>
+            <Container className='user-info-container'>
+                <Container className='mt-4 profile-container'>
+                    <div className='user-info'>
+                        <Row className='mt-2'>
+                            <Col className='label'>Name: <span>{user?.name}</span></Col>
+                        </Row>
+                        <Row className='mt-2'>
+                            <Col className='label'>Email: <span>{user?.email}</span></Col>
+                        </Row>
+                        <Row className='mt-2'>
+                            <Col className='label'>Token: <span>{user?.token}</span></Col>
+                        </Row>
+                        <div className='edit-btn'>
+                            <Button
+                                className='mt-4 d-flex align-items-center justify-content-center'
+                                variant="outline-primary"
+                                onClick={() => setModalShow(true)}>
+                                Edit
+                            </Button>
+                        </div>
+                    </div>
+                </Container>
             </Container>
-            <div className='edit-btn'>
-                <Button
-                    className='mt-4 d-flex align-items-center justify-content-center'
-                    variant="outline-primary"
-                    onClick={() => setModalShow(true)}>
-                    Edit
-                </Button>
-            </div>
-
             <MyModal contentTitle='Update Profile' show={modalShow} onHide={() => setModalShow(false)}>
                 <UpdateProfile user={user} setUser={setUser} onHide={() => setModalShow(false)} />
             </MyModal>
